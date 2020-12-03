b8ee89e431fd2c39efb1f2ad07b1ca59b23daa91
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2020-12-03 16:52:05 +0800

    fixed block data

--- app/routes/api/blocks.js
+++ app/routes/api/blocks.js
@@ -9,8 +9,11 @@ router.get('/', (req, res) => res.json(bc.chain));
 
 router.get('/:nonce', (req, res) => res.json(bc.chain[req.params.nonce]));
 
+/// It is required to have this headers given below
+/// Content-Type: application/json
+/// To be able to parse the response as a json
 router.post('/mine', (req, res) => {
-    const block = bc.addBlock(req.body);
+    const block = bc.addBlock(req.body.data);
     console.log(`New block added: ${block.toString()}`);
 
     res.redirect('/blocks');
